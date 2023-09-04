6295ebb4eff27c0d850aacf8f22537e2f05db091
clientemercado <edwilsoncurti@hotmail.com>
2023-09-04 17:03:55 -0300

    - Traduzida e alterada a mensagem para confirma;áo da segunda senha, que tem q ser identica a primeira;

--- Frameworks/Repository.Pattern/Scripts/jquery.validate.js
+++ Frameworks/Repository.Pattern/Scripts/jquery.validate.js
@@ -288,7 +288,7 @@ $.extend($.validator, {
 		number: "Please enter a valid number.",
 		digits: "Please enter only digits.",
 		creditcard: "Please enter a valid credit card number.",
-		equalTo: "Please enter the same value again.",
+		equalTo: "A senha de confirmação está diferente da primeira.",
 		maxlength: $.validator.format("Please enter no more than {0} characters."),
 		minlength: $.validator.format("Please enter at least {0} characters."),
 		rangelength: $.validator.format("Please enter a value between {0} and {1} characters long."),
